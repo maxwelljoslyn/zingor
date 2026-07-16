e1011a2fc581518114616074f11cc306ef9a916a
Maxwell Joslyn <maxwelljoslyn@gmail.com>
2026-07-15 22:10:25 -0500

    finish documentation setup
    Refs #92

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,13 @@ import os
 
 extensions = [
     "sphinx_rtd_theme",
+    "myst_parser",
 ]
 
 html_theme = "sphinx_rtd_theme"
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+project = "Zingor"
+author = "Maxwell Joslyn"
+project_copyright = "%Y Maxwell Joslyn"
+
+myst_enable_extensions = ["colon_fence"]
--- justfile
+++ justfile
@@ -1,7 +1,10 @@
-default: fix format
+default: fix format docs
 
 fix:
     uv run ruff check --fix
 
 format:
     uv run ruff format
+
+docs:
+    uv run python -m sphinx -T -b html docs docs/_build/html
